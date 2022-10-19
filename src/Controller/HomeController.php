82c805a51a4d6ee2fc0877911aeec3c6820b5740
Martin <martin@email.com>
2022-10-19 09:48:18 +0200

    Entité série, test entité dans controlleur home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
 use App\Service\PdoFouDeSerie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,4 +30,20 @@ class HomeController extends AbstractController
         $nombreSeries = $pdoFouDeSerie->getNbSeries();
         return $this->render('lesSeries/lesseries.html.twig', ['lesSeries' => $truc , 'nombreSeries' => $nombreSeries]);
     }
+
+    // #[Route('/testEntity', name: 'app_testentity')]
+    // public function testEntity(ManagerRegistry $doctrine): Response
+    // {
+    //     $serie = new Serie();
+    //     $serie->setTitre('ok');
+    //     $serie->setResume('ok');
+    //     $serie->setDuree(new \DateTime('00:30:00'));
+    //     $serie->setPremiereDiffusion(new \DateTime('01-09-2022'));
+    //     $serie->setImage('');
+    //     $entityManager=$doctrine->getManager();
+    //     $entityManager->persist($serie);
+    //     $entityManager->flush();
+
+    //     return $this->render('home/testEntity.html.twig', ['serie' => $serie]);
+    // }
 }
